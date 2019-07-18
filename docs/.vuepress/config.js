a90e3123aa08e987b35490a3f8548ffd36452551
KannadukiAme <misuzunagisa@gmail.com>
2019-07-18 09:13:44 +0800

    添加favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,8 @@ module.exports = {
   title: 'kksk',
   description: 'some notes for web dev...',
   head: [
-    ['link', { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.8.1/css/all.css', integrity: 'sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf', crossorigin: 'anonymous' }]
+    ['link', { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.8.1/css/all.css', integrity: 'sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf', crossorigin: 'anonymous' }],
+    ['link', { rel: 'icon', href:'/madoka-avatar.png'}]
   ],
   base: '/kksk/',
   dest: './dist',
