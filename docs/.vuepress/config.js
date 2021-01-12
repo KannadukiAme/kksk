935738448ce648cdcad3da4d449cb7adfbd4403f
KannadukiAme <misuzunagisa@gmail.com>
2021-01-12 11:25:37 +0800

    add win note

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -72,6 +72,14 @@ module.exports = {
           '/notes/web-server/jellyfin',
         ]
       },
+      {
+        title: 'Windows',   // 必要的
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 2,    // 可选的, 默认值是 1
+        children: [
+          '/notes/win/win'
+        ]
+      },
     ],
     lastUpdated: '最近更新'
   },
